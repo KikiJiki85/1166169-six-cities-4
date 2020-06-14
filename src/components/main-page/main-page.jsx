ba3f0a0ec22f5ec7f539321a8a60ce8e50a3e82c
Aleksey Vaskin <alzor85@gmail.com>
2020-06-14 19:13:26 +0300

    Добавит правку по замечаниям наставника

--- src/components/main-page/main-page.jsx
+++ src/components/main-page/main-page.jsx
@@ -86,12 +86,14 @@ const MainPage = (props) => {
                   <li className="places__option" tabIndex="0">Top rated first</li>
                 </ul>
 
-                <select className="places__sorting-type" id="places-sorting">
-                  <option className="places__option" value="popular" defaultValue="">Popular</option>
-                  <option className="places__option" value="to-high">Price: low to high</option>
-                  <option className="places__option" value="to-low">Price: high to low</option>
-                  <option className="places__option" value="top-rated">Top rated first</option>
+                {/*
+                <select class="places__sorting-type" id="places-sorting">
+                  <option class="places__option" value="popular" selected="">Popular</option>
+                  <option class="places__option" value="to-high">Price: low to high</option>
+                  <option class="places__option" value="to-low">Price: high to low</option>
+                  <option class="places__option" value="top-rated">Top rated first</option>
                 </select>
+               */}
 
               </form>
               <div className="cities__places-list places__list tabs__content">

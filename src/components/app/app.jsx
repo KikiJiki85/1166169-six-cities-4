9c950ecfa2e22f2270599494f63fa69edfe57603
Aleksey Vaskin <alzor85@gmail.com>
2020-06-25 07:46:41 +0300

    Добавит ReactRouter в проект и компонент Property

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,36 @@
-import React from "react";
-import MainPage from "../main-page/main-page.jsx";
+import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
+import {Route, BrowserRouter, Switch} from "react-router-dom";
+
+import MainPage from "../main-page/main-page.jsx";
+import PropertyPage from "../property-page/property-page.jsx";
 
 const headerClickHandler = () => {};
 
-const App = ({placesToStay, offers}) => {
-  return (
-    <MainPage placesToStay={placesToStay} offers={offers} onHeaderClick={headerClickHandler}/>
-  );
-};
+export default class App extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    const {placesToStay, offers} = this.props;
+
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <MainPage placesToStay={placesToStay} offers={offers} onHeaderClick={headerClickHandler}/>
+          </Route>
+          <Route exact path="/property">
+            <PropertyPage />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
 
 App.propTypes = {
   placesToStay: PropTypes.number.isRequired,
@@ -24,4 +46,3 @@ App.propTypes = {
   )
 };
 
-export default App;

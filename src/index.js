eb2db3cbdf33c406f83a75c54f6a01b7b22448e4
Aleksey Vaskin <alzor85@gmail.com>
2020-07-10 15:47:09 +0300

    Подключит redux. Добавит Reducer и provider.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+import {reducer} from "./reducer.js";
 import App from "./components/app/app.jsx";
 import offers from "./mocks/offers.js";
 import users from "./mocks/users.js";
@@ -8,11 +11,15 @@ const Settings = {
   PLACES_TO_STAY: 333,
 };
 
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f);
+
 ReactDOM.render(
-    <App
-      placesToStay = {Settings.PLACES_TO_STAY}
-      offers = {offers}
-      users = {users}
-    />,
+    <Provider store = {store}>
+      <App
+        placesToStay = {Settings.PLACES_TO_STAY}
+        offers = {offers}
+        users = {users}
+      />,
+    </Provider>,
     document.querySelector(`#root`)
 );

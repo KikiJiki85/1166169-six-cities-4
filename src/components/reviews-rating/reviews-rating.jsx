4367f650a13fe174b944e50a46def0dc09bc3819
Aleksey Vaskin <alzor85@gmail.com>
2020-08-02 11:10:41 +0300

    Правка по замечаниям

--- src/components/reviews-rating/reviews-rating.jsx
+++ src/components/reviews-rating/reviews-rating.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 const RATING_STARS_TEMPLATE = [5, 4, 3, 2, 1];
 
-const ratingToTitle = {
+const RatingToTitle = {
   5: `perfect`,
   4: `good`,
   3: `not bad`,
@@ -22,7 +22,7 @@ const ReviewsRating = (props) => {
           return (
             <React.Fragment key={num}>
               <input onChange={onRatingChange} disabled={isFormDisabled} checked={rating === String(num)} className="form__rating-input visually-hidden" name="rating" value={num} id={id} type="radio"/>
-              <label htmlFor={id} className="reviews__rating-label form__rating-label" title={ratingToTitle[num]}>
+              <label htmlFor={id} className="reviews__rating-label form__rating-label" title={RatingToTitle[num]}>
                 <svg className="form__star-image" width="37" height="33">
                   <use xlinkHref="#icon-star"></use>
                 </svg>
